1e9ec0b0332c530975b92575766dc810112c9751
Minh Đặng Công <minh.dang@codecomplete.jp>
2020-03-30 15:27:08 +0700

    Init project

---
+++ device_management/lib/app/model/core/AppComponent.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:device_management/app/model/core/AppProvider.dart';
+import 'package:device_management/app/model/core/AppStoreApplication.dart';
+import 'package:device_management/config/Env.dart';
+import 'package:device_management/generated/i18n.dart';
+import 'package:device_management/utility/log/Log.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+
+class AppComponent extends StatefulWidget {
+
+  final AppStoreApplication _application;
+
+  AppComponent(this._application);
+
+  @override
+  State createState() {
+    return new AppComponentState(_application);
+  }
+}
+
+class AppComponentState extends State<AppComponent> {
+
+  final AppStoreApplication _application;
+
+  AppComponentState(this._application);
+
+  @override
+  void dispose()async{
+    Log.info('dispose');
+    super.dispose();
+    await _application.onTerminate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final app = new MaterialApp(
+        title: Env.value.appName,
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        onGenerateRoute: _application.router.generator,
+    );
+    print('initial core.route = ${app.initialRoute}');
+
+    final appProvider = AppProvider(child: app, application: _application);
+    return appProvider;
+  }
+}
